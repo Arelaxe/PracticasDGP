007c49c2c628db58814a07695100f400b45b176d
pserrano778 <58224411+pserrano778@users.noreply.github.com>
2020-10-20 21:02:10 +0200

    Login corregido

---
+++ API-rest/API-BaseDeDatos_c-01.js
@@ -0,0 +1,73 @@
+require('dotenv').config();
+
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL = process.env.MONGO_CONNECTION_URL;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+
+var app = Express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+// var jsonParser = BodyParser.json();
+
+var database, collectionUsuarios;
+
+app.listen(5000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collectionUsuarios = database.collection("Usuarios");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+/******************************************************/
+// Registro Usuarios
+/******************************************************/
+app.post("/registro", (request, response) => {
+    collectionUsuarios.insertOne(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        if (result == null){
+            response.send("Registro incorrecto");
+        }
+        else{    
+            response.send("Registro completado");
+        }
+    });
+});
+
+/******************************************************/
+// Login Usuarios
+/******************************************************/
+app.post("/login", (request, response) => {
+
+    collectionUsuarios.findOne({ "email":request.body.email }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        if (result == null){
+            response.send("Nombre de usuario incorrecto");
+        }
+        else{
+            collectionUsuarios.findOne({ "email":request.body.email,"password":request.body.passwd }, (errorPass, resultPass) => {
+                if(errorPass) {
+                    return response.status(500).send(errorPass);
+                }
+                if (resultPass == null){
+                    response.send("Contraseña incorrecta");
+                }
+                else{
+                    console.log(result.rol);
+                    response.send(result.rol); //Si ha iniciado sesión correctamente, se le responde con su rol
+                }
+            }
+        );
+        }
+    });
+});
