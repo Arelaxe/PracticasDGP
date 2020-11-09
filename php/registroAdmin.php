4fdb487084d828b6ebfed8ee97a74b803521462e
javiercdag <58362922+javiercdag@users.noreply.github.com>
2020-11-09 12:55:04 +0100

    modal de error y redireccion correcta

--- php/registroAdmin.php
+++ php/registroAdmin.php
@@ -5,6 +5,7 @@
     if(isset($_SESSION['usuario']) && ($_SESSION['rol'] == "admin" || $_SESSION['rol'] == "ambos")){
      if($_SERVER['REQUEST_METHOD'] === "POST"){
         $infoRegistro = array();
+        $sociosACargo = array();
 
         $infoRegistro['nombre'] = $_POST['nombre'];
         $infoRegistro['username'] = $_POST['email'];
@@ -12,10 +13,19 @@
         $infoRegistro['rol'] = $_POST['rol'];
         $infoRegistro['direccion'] = $_POST['direccion'];
         $infoRegistro['telefono'] = $_POST['telefono'];
+        $infoRegistro['sociosACargo'] = $sociosACargo;
 
         $jsoninfoRegistro = json_encode($infoRegistro);
 
         $result = registroApi($jsoninfoRegistro);
+        $result = json_decode($result);
+
+        if ($result->name == "MongoError" && $result->code == 11000){
+            header("Location: ../registro.php?error=11000");  
+		}
+        else{
+            header("Location: ../perfil.php?username=".$infoRegistro['username']);  
+		}
     }
     }
 
