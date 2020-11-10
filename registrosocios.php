a495ad2a45e241e3b6c2be258e92c15eee44c89b
javiercdag <58362922+javiercdag@users.noreply.github.com>
2020-11-10 18:18:44 +0100

    registrosocios.php valida primary key

--- registrosocios.php
+++ registrosocios.php
@@ -6,10 +6,16 @@
     $loader = new \Twig\Loader\FilesystemLoader('templates');
     $twig = new \Twig\Environment($loader);
     
+    $error = false;
+
     if(isset($_SESSION['usuario'])){     
         $rol = $_SESSION['rol'];
+
+        if (isset($_GET['error']))
+                if ($_GET['error']=="11000")
+                    $error = true;
     }
     else $rol = "";
 
-    echo $twig->render('registrosocios.html', ['rol' => $rol]);
+    echo $twig->render('registrosocios.html', ['rol' => $rol, 'error' => $error]);
 ?>
