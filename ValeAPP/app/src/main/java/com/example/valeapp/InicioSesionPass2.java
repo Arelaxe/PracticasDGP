7f4496bebbde2fecab2a36cddbf6669cff53063e
ErMiguel99 <61285238+ErMiguel99@users.noreply.github.com>
2020-11-14 13:07:36 +0100

    iteracion 2 terminada

--- ValeAPP/app/src/main/java/com/example/valeapp/InicioSesionPass2.java
+++ ValeAPP/app/src/main/java/com/example/valeapp/InicioSesionPass2.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InicioSesionPass2 extends AppCompatActivity {
