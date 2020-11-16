70edfcb36f9cb151782067ee9a48280d23c1ae96
franmolsan <56502397+franmolsan@users.noreply.github.com>
2020-11-16 20:17:53 +0100

    cambios finales

--- ValeAPP/app/src/main/java/com/example/valeapp/InicioSesionNombreUsuario.java
+++ ValeAPP/app/src/main/java/com/example/valeapp/InicioSesionNombreUsuario.java
@@ -117,4 +117,13 @@ public class InicioSesionNombreUsuario extends AppCompatActivity {
         intent.putExtra("usuario", usuario);
         startActivity(intent);
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+
+    }
 }
