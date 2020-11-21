98c5618fd2425646c82a067601ebf88b8280337c
Noelia Escalera <36896793+Arelaxe@users.noreply.github.com>
2020-11-21 20:06:13 +0100

    enviar tarea

--- operaciones_api/autenticacionApi.php
+++ operaciones_api/autenticacionApi.php
@@ -75,14 +75,6 @@ function listadoSociosApi(){
      return $response;
 }
 
-function listadoTareasAdminApi(){
-    // API URL
-     $url = 'http://localhost:5000/listado-tareas-admin';
-
-     $response = json_decode(file_get_contents($url));
-     return $response;
-}
-
 function infoPerfilApi($jsonInfoPerfil){
     // API URL
      $url = 'http://localhost:5000/perfil';
@@ -761,6 +753,31 @@ function editarTareaApi($jsoninfoTarea){
     // Close cURL resource
     curl_close($ch);
     
+    return $result;
+}
+
+function enviarTareaApi($jsoninfoTarea){
+    // API URL
+    $url = 'http://localhost:5000/registro';
+
+    // Create a new cURL resource
+    $ch = curl_init($url);
+
+    // Attach encoded JSON string to the POST fields
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsoninfoTarea);
+
+    // Set the content type to application/json
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+    // Return response instead of outputting
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Execute the POST request
+    $result = curl_exec($ch);
+
+    // Close cURL resource
+    curl_close($ch);
+    print($result);
     return $result;
 }
 ?>
