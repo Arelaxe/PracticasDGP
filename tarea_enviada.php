28fb45b2892b0c07b695b47bd14c2a7e2c6e6b03
IvanitiX <IvanVR@protonmail.com>
2020-11-29 13:30:48 +0100

    Template de Tarea Enviada

---
+++ tarea_enviada.php
@@ -0,0 +1,44 @@
+<?php
+    require_once '../vendor/autoload.php';
+    require_once 'php/mostrarFotoPerfil.php';
+    include_once("guardarImagen.php");
+
+    $loader = new \Twig\Loader\FilesystemLoader('templates');
+    $twig = new \Twig\Environment($loader);
+
+    session_start();
+
+    if(isset($_SESSION['usuario'])) $rol = $_SESSION['rol'];
+    else $rol = "";
+
+    if (isset($_GET['nombre'])) {
+        $idUsuario = $_SESSION['usuario'];
+
+        $idTarea = array();
+        $idTarea['nombre'] = $_GET['nombre'];
+        isset($_GET['creador']) ? $idTarea['creador'] = $_GET['creador'] : $idTarea['creador'] = $idUsuario;
+
+        $jsonIdTarea = json_encode($idTarea);
+
+        $result = infoTareaApi($jsonIdTarea);
+
+        $result = json_decode($result);
+
+        $infoTarea = array();
+
+        $infoTarea['nombre'] = $result[0]->nombre;
+        $infoTarea['creador'] = $result[0]->creador;
+        $infoTarea['descripcion'] = $result[0]->descripcion;
+        getImage($result[0]->fotoTarea);
+        $infoTarea['fotoTarea'] = "img/".$result[0]->fotoTarea;
+        getImage($result[0]->videoTarea);
+        $infoTarea['videoTarea'] = "img/".$result[0]->videoTarea;
+        getImage($result[0]->audioTarea);
+        $infoTarea['audioTarea'] = "img/".$result[0]->audioTarea;
+    } 
+    else{
+        $idUsuario = -1;
+    }
+
+    echo $twig->render('tareaEnviada.html',['infoTarea' => $infoTarea ,'rol' => $rol, 'cuenta' => $_SESSION['usuario'], 'img' => fotoPerfil($_SESSION['usuario'])]);
+?>
