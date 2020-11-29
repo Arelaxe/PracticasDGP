6aa078a8069ddeabcfbdf877599a859d45083cee
franmolsan <56502397+franmolsan@users.noreply.github.com>
2020-11-29 11:41:42 +0100

    ahora funciona descargarse los videos

--- ValeAPP/app/src/main/java/com/example/valeapp/MainActivity.java
+++ ValeAPP/app/src/main/java/com/example/valeapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.valeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -11,6 +15,13 @@ import android.os.SystemClock;
 public class MainActivity extends AppCompatActivity {
 
     String nombreUsuario;
+
+    // Guardar permisos
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     private Runnable taskNombreUsuario = new Runnable() {
         public void run() {
@@ -34,15 +45,15 @@ public class MainActivity extends AppCompatActivity {
         nombreUsuario = infoUsuario.getData(mContext);
 
         Handler handler = new Handler();
-
+        verifyStoragePermissions(this);
 
         if(nombreUsuario == null) {
             //Login Usuario
-            handler.postDelayed(taskNombreUsuario, 1500);
+            handler.postDelayed(taskNombreUsuario, 3000);
         }
         else {
             // Login contraseña
-            handler.postDelayed(taskContraseña, 1500);
+            handler.postDelayed(taskContraseña, 3000);
         }
 
 
@@ -66,5 +77,26 @@ public class MainActivity extends AppCompatActivity {
         a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(a);
 
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 }
