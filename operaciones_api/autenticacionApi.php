4021f63830dfecf076599325fabb4a45074cdf64
Iván V.R <37305539+IvanitiX@users.noreply.github.com>
2020-12-09 17:44:26 +0100

    Add files via upload

--- operaciones_api/autenticacionApi.php
+++ operaciones_api/autenticacionApi.php
@@ -898,4 +898,54 @@ function borrarChatApi($jsonInfoChat){
     return $result;
 }
 
+function nuevoMensajeWebApi($jsoninfoTarea){
+    // API URL
+    $url = 'http://localhost:5000/nuevo-mensaje-web';
+
+    // Create a new cURL resource
+    $ch = curl_init($url);
+
+    // Attach encoded JSON string to the POST fields
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsoninfoTarea);
+
+    // Set the content type to application/json
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+    // Return response instead of outputting
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Execute the POST request
+    $result = curl_exec($ch);
+
+    // Close cURL resource
+    curl_close($ch);
+    print($result);
+    return $result;
+}
+
+function vistaTareaWebApi($jsoninfoTarea){
+    // API URL
+    $url = 'http://localhost:5000/tarea-vista-web';
+
+    // Create a new cURL resource
+    $ch = curl_init($url);
+
+    // Attach encoded JSON string to the POST fields
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsoninfoTarea);
+
+    // Set the content type to application/json
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+    // Return response instead of outputting
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Execute the POST request
+    $result = curl_exec($ch);
+
+    // Close cURL resource
+    curl_close($ch);
+    print($result);
+    return $result;
+}
+
 ?>
